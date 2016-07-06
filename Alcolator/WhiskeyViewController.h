0c06a5f0fc9ecac52ff9e05bb9af00c7831d7bc7
stephenkais <stephenkais@gmail.com>
2016-07-06 16:52:05 -0700

    Checkpoint 20: completed

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Stephen Kaiser on 7/6/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
