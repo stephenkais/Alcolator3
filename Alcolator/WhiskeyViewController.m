165eff2af6c748c440eeb2e4df75bac864fd22bf
stephenkais <stephenkais@gmail.com>
2016-07-06 21:07:59 -0700

    Assignment 21 completed

--- Alcolator/WhiskeyViewController.m
+++ Alcolator/WhiskeyViewController.m
@@ -47,6 +47,7 @@
         whiskeyText = NSLocalizedString(@"shots", @"plural of shot");
     }
     NSString *resultText = [NSString stringWithFormat:NSLocalizedString(@"%d %@ (with %.2f%% alcohol) contains as much alcohol as %.1f %@ of whiskey.", nil), numberOfBeers, beerText, [self.beerPercentTextField.text floatValue], numberOfWhiskeyGlassesForEquivalentAlcoholAmount, whiskeyText];
+    self.navigationItem.title = [NSString stringWithFormat:@"%f shots", numberOfWhiskeyGlassesForEquivalentAlcoholAmount];
     self.resultLabel.text = resultText;
 }
 
